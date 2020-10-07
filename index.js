cb8db0b62c85632b6690806ef8d1d6e012e91873
Nathan Krasney <natankrasney@gmail.com>
2020-10-07 11:13:55 +0300

    add exit and comment

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ console.log('app is loading');
 // you get SIGINT when you do pm2 stop
 process.on('SIGINT', () => {
     console.log('Received SIGINT');
-    process.exit(0);
+    /*If your application does not exit by itself before 1.6s (customizable)
+     it will receive a SIGKILL signal to force the process exit.*/
+     process.exit(0)
   });
 
 setInterval(() => console.log(new Date()),1000)
