6709fac818e38ca860922cb555e366459d4746c6
Nathan Krasney <natankrasney@gmail.com>
2020-10-07 11:09:26 +0300

    add comment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log('app is loading');
 
+// you get SIGINT when you do pm2 stop
 process.on('SIGINT', () => {
-    console.log('Received SIGINT. Press Control-D to exit.');
+    console.log('Received SIGINT');
+    process.exit(0);
   });
 
 setInterval(() => console.log(new Date()),1000)
