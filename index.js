926107b086ca1a50010a143a4a45c041a1c023c0
Nathan Krasney <natankrasney@gmail.com>
2020-10-07 11:16:15 +0300

    linux comment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('app is loading');
 
 // you get SIGINT when you do pm2 stop
+// SIGINT exist on linux not windows
 process.on('SIGINT', () => {
     console.log('Received SIGINT');
     /*If your application does not exit by itself before 1.6s (customizable)
